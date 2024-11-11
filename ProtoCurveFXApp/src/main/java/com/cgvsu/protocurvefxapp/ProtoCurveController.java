30150b80dabb62ec64392fd7bdf08d94e21991da
lein3000zzz <146432991+lein3000zzz@users.noreply.github.com>
2024-11-11 20:16:43 +0300

    Additional algorithms

--- ProtoCurveFXApp/src/main/java/com/cgvsu/protocurvefxapp/ProtoCurveController.java
+++ ProtoCurveFXApp/src/main/java/com/cgvsu/protocurvefxapp/ProtoCurveController.java
@@ -10,9 +10,6 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
 
 public class ProtoCurveController {
 
@@ -22,7 +19,7 @@ public class ProtoCurveController {
     private Canvas canvas;
     final int POINT_RADIUS = 3;
     private Point2D draggedPoint = null;
-
+    public ArrayList<Point2D> points = new ArrayList<>();
     @FXML
     private void initialize() {
         anchorPane.prefWidthProperty().addListener((ov, oldValue, newValue) -> canvas.setWidth(newValue.doubleValue()));
@@ -39,7 +36,7 @@ public class ProtoCurveController {
                 draggedPoint = new Point2D(event.getX(), event.getY());
                 points.add(draggedPoint);
             }
-            redraw(canvas.getGraphicsContext2D());
+            drawPoints(canvas.getGraphicsContext2D(), points);
         });
 
         canvas.setOnMouseReleased(event -> {
@@ -50,62 +47,127 @@ public class ProtoCurveController {
             if (findPointNearby(event.getX(), event.getY()) == null) {
                 createPointWithXCheck(event);
             }
-            redraw(canvas.getGraphicsContext2D());
+            redraw(canvas.getGraphicsContext2D(), points);
             draggedPoint = null;
         });
     }
 
-    private static final double MIN_DISTANCE_HOLDER = 0.3;
-    public static ArrayList<Point2D> points = new ArrayList<>();
-
-    public void redraw(GraphicsContext graphicsContext) {
+    public void redraw(GraphicsContext graphicsContext, ArrayList<Point2D> points) {
         graphicsContext.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+        drawPoints(graphicsContext, points);
+        ArrayList<Point2D> interpolatedPoints = calculateLagrangePoints(points);
+        drawBresenhamLine(graphicsContext, interpolatedPoints);
+    }
+
+    private void drawPoints(GraphicsContext graphicsContext, ArrayList<Point2D> points) {
         final int POINT_RADIUS = 3;
         for (Point2D point : points) {
             graphicsContext.fillOval(
                     point.getX() - POINT_RADIUS, point.getY() - POINT_RADIUS,
                     2 * POINT_RADIUS, 2 * POINT_RADIUS);
         }
+    }
 
+    private ArrayList<Point2D> calculateLagrangePoints(ArrayList<Point2D> points) {
+        ArrayList<Point2D> interpolatedPoints = new ArrayList<>();
+        for (double x = 0; x < canvas.getWidth(); x += 5) {
+            double y = LagrangeInterpolation.interpolate(x, points);
+            if (foundPointOnTheSameX(x)) {
+                y = getPointYFromTheSameX(x);
+            }
+            checkYReq(y, 0, canvas.getHeight());
+            interpolatedPoints.add(new Point2D(x, y));
+        }
+        return interpolatedPoints;
+    }
+
+    private void drawBresenhamLine(GraphicsContext graphicsContext, ArrayList<Point2D> points) {
         PixelWriter pixelWriter = graphicsContext.getPixelWriter();
-        if (points.size() > 1) {
-            graphicsContext.setLineWidth(0.75);
-            Point2D prevXY = null;
-            for (double x = 0; x < canvas.getWidth(); x += 0.5) {
-                double y = LagrangeInterpolation.interpolate(x, points);
-                if (findPointOnTheSameX(x))
-                    y = getPointYFromTheSameX(x);
-                y = checkYReq(y);
-                if (prevXY != null) {
-                    graphicsContext.strokeLine(x, y, prevXY.getX(), prevXY.getY());
+        for (int i = 0; i < points.size() - 1; i++) {
+            Point2D start = points.get(i);
+            Point2D end = points.get(i + 1);
+
+            int x0 = (int) Math.round(start.getX());
+            int y0 = (int) Math.round(start.getY());
+            int x1 = (int) Math.round(end.getX());
+            int y1 = (int) Math.round(end.getY());
+
+            int dx = Math.abs(x1 - x0);
+            int dy = Math.abs(y1 - y0);
+
+            int sx = x0 < x1 ? 1 : -1;
+            int sy = y0 < y1 ? 1 : -1;
+            int err = dx - dy;
+
+            while (true) {
+                pixelWriter.setColor(x0, y0, Color.RED);
+
+                if (x0 == x1 && y0 == y1) {
+                    break;
                 }
-//                pixelWriter.setColor((int) Math.round(x), (int) Math.round(y), Color.RED);
-//                System.out.println(prevXY);
-//                System.out.println("X " + x + " Y " + y);
-                prevXY = new Point2D(x, y);
+
+                int e2 = 2 * err;
+
+                if (e2 > -dy) {
+                    err -= dy;
+                    x0 += sx;
+                }
+
+                if (e2 < dx) {
+                    err += dx;
+                    y0 += sy;
+                }
             }
         }
-//        System.out.println(points.get(0));
-//        System.out.println(points);
     }
+
+
+//    public void redraw(GraphicsContext graphicsContext) {
+//        graphicsContext.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+//        final int POINT_RADIUS = 3;
+//        for (Point2D point : points) {
+//            graphicsContext.fillOval(
+//                    point.getX() - POINT_RADIUS, point.getY() - POINT_RADIUS,
+//                    2 * POINT_RADIUS, 2 * POINT_RADIUS);
+//        }
+//
+//        PixelWriter pixelWriter = graphicsContext.getPixelWriter();
+//        if (points.size() > 1) {
+//            graphicsContext.setLineWidth(0.75);
+//            Point2D prevXY = null;
+//            for (double x = 0; x < canvas.getWidth() * 2; x += 1) {
+//                double y = LagrangeInterpolation.interpolate(x, points);
+//                y = checkYReq(y);
+//                if (prevXY != null) {
+//                    graphicsContext.strokeLine(x, y, prevXY.getX(), prevXY.getY());
+//                }
+////                pixelWriter.setColor((int) Math.round(x), (int) Math.round(y), Color.RED);
+////                System.out.println(prevXY);
+////                System.out.println("X " + x + " Y " + y);
+//                prevXY = new Point2D(x, y);
+//            }
+//        }
+////        System.out.println(points.get(0));
+////        System.out.println(points);
+//    }
 
     private void createNewPoint(GraphicsContext graphicsContext, MouseEvent event) {
         points.add(new Point2D(event.getX(), event.getY()));
-        redraw(graphicsContext);
+        redraw(graphicsContext, points);
     }
 
-    private double checkYReq(double y) {
-        if (y < 0)
-            return -1;
-        else if (y > canvas.getHeight())
-            return canvas.getHeight() + 1;
+    private double checkYReq(double y, double lowerBorder, double upperBorder) {
+        if (y < lowerBorder)
+            return lowerBorder - 1;
+        else if (y > upperBorder)
+            return upperBorder + 1;
         else
             return y;
     }
 
     private void createNewPointByXY(GraphicsContext graphicsContext, double x, double y) {
         points.add(new Point2D(x, y));
-        redraw(graphicsContext);
+        redraw(graphicsContext, points);
     }
     private Point2D findPointNearby(double x, double y) {
         for (Point2D point : points) {
@@ -115,7 +177,7 @@ public class ProtoCurveController {
         }
         return null;
     }
-    private boolean findPointOnTheSameX(double x) {
+    private boolean foundPointOnTheSameX(double x) {
         for (Point2D point : points) {
             if (x == point.getX()) {
                 return true;
@@ -133,24 +195,24 @@ public class ProtoCurveController {
         return Double.POSITIVE_INFINITY;
     }
 
-    private boolean findPointOnTheSameY(double y) {
-        for (Point2D point : points) {
-            if (y == point.getY()) {
-                return true;
-            }
-        }
-        return false;
-    }
+//    private boolean findPointOnTheSameY(double y) {
+//        for (Point2D point : points) {
+//            if (y == point.getY()) {
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
 
     private void createPointWithXCheck(MouseEvent event) {
-        if (!findPointOnTheSameX(event.getX()))
+        if (!foundPointOnTheSameX(event.getX()))
             createNewPoint(canvas.getGraphicsContext2D(), event);
         else {
             for (int x = 1; x < canvas.getWidth(); x++) {
-                if (!findPointOnTheSameX(event.getX() + x)) {
+                if (!foundPointOnTheSameX(event.getX() + x)) {
                     createNewPointByXY(canvas.getGraphicsContext2D(), event.getX() + x, event.getY());
                     break;
-                } else if ((!findPointOnTheSameX(event.getX() - x))) {
+                } else if ((!foundPointOnTheSameX(event.getX() - x))) {
                     createNewPointByXY(canvas.getGraphicsContext2D(), event.getX() - x, event.getY());
                     break;
                 }
